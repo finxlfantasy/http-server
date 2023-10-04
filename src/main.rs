ff32c005ce69812693b6669a69dae750a68697b0
luciano-eth <113019900+luciano-eth@users.noreply.github.com>
2023-10-04 21:49:57 +0200

    pass 4th stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{TcpStream, TcpListener};
 use std::io::{Read, Write};
+use std::vec;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -28,4 +29,49 @@ pub fn handle_request(mut stream: TcpStream) {
         stream.write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes()).unwrap();
     }
     println!("Request: {}", request);
+}
+
+pub struct Request {
+    pub method: String,
+    pub path: String,
+    pub http_version: String,
+}
+
+pub struct Response {
+    pub status_code: u16,
+    pub status_text: String,
+    pub headers: Vec<(String, String)>,
+    pub body: String,
+}
+
+impl Response {
+    pub fn new(status_code: u16, status_text: String, body: String) -> Response {
+        Response {
+            status_code, 
+            status_text,
+            headers: vec![
+                ("Content-Type".to_string(), "text/plain".to_string()),
+                ("Content-Length".to_string(), body.len().to_string()),
+            ],
+            body,
+        }
+    }
+
+    pub fn add_headers(mut self, name: String, value: String) {
+        self.headers.push((name, value));
+    }
+
+
+    pub fn to_string(&self) -> String {
+        let mut header_string = format!("HTTP/1.1 {} {}\r\n", self.status_code, self.status_text);
+
+        for (name, value) in &self.headers {
+            header_string.push_str(&format!("{}: {}\r\n", name, value));
+        }
+
+        header_string.push_str("\r\n");
+        header_string.push_str(&self.body);
+
+        header_string
+    }
 }
