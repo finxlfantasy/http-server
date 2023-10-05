863906123e28ffe9d9fc1433858727fd39ec8b92
luciano-eth <113019900+luciano-eth@users.noreply.github.com>
2023-10-05 09:25:32 +0200

    pass 5th stage

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ use std::vec;
 
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
